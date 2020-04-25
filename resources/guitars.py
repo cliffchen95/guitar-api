25a8a4450ca71055a1eb5f96199caee91ce40b71
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-25 17:59:14 -0400

    adding guitar through post route

--- resources/guitars.py
+++ resources/guitars.py
@@ -1,6 +1,7 @@
 import models
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
 
 guitars = Blueprint('guitars', 'guitars')
 
@@ -9,8 +10,13 @@ def hello_guitar():
   return "Hello guitar!"
 
 @guitars.route('/', methods=['POST'])
-def create_dog():
+def create_guitar():
   payload = request.get_json()
-  print(payload)
-  
-  return "you hit post"
+  new_guitar = models.Guitar.create(name=payload['name'], is_electric=payload['is_electric'], price=payload['price'])
+  print(new_guitar)
+  guitar_dict = model_to_dict(new_guitar)
+  return jsonify(
+      data=guitar_dict,
+      message="Successfully created dog!",
+      status=201
+    ), 201
