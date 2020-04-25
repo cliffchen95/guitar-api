4a665717e6f28dceed95edc3a8ac56b96b69ca79
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-25 17:29:18 -0400

    database connected

---
+++ models.py
@@ -0,0 +1,21 @@
+from peewee import *
+import datetime
+
+
+DATABASE = SqliteDatabase('guitar.sqlite')
+
+class Guitar(Model):
+  name = CharField()
+  is_electric = BooleanField()
+  price = DoubleField()
+
+  class Meta:
+    database = DATABASE
+
+def initialize():
+  DATABASE.connect()
+
+  DATABASE.create_tables([Guitar], safe=True)
+  print('connect to database and create tables')
+
+DATABASE.close()
