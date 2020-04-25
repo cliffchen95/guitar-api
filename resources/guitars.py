6a10dc4811a62b77e1833c5e42ae7f941a0676b0
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-25 18:10:20 -0400

    GET route show all guitars

--- resources/guitars.py
+++ resources/guitars.py
@@ -7,13 +7,24 @@ guitars = Blueprint('guitars', 'guitars')
 
 @guitars.route('/', methods=['GET'])
 def hello_guitar():
-  return "Hello guitar!"
+  result = models.Guitar.select()
+  data = []
+  count = 0
+  for guitar in result:
+    count += 1
+    data.append(model_to_dict(guitar))
+  return jsonify(
+    data=data,
+    message=f"Successfully found {count} results",
+    status=200
+    ), 200
+
 
 @guitars.route('/', methods=['POST'])
 def create_guitar():
   payload = request.get_json()
   new_guitar = models.Guitar.create(name=payload['name'], is_electric=payload['is_electric'], price=payload['price'])
-  print(new_guitar)
+  
   guitar_dict = model_to_dict(new_guitar)
   return jsonify(
       data=guitar_dict,
