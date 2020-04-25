6a7dc7a9589b81c7bdecc78fa23fc4a70b15a1f1
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-25 18:20:56 -0400

    finding guitar by index

--- resources/guitars.py
+++ resources/guitars.py
@@ -1,13 +1,13 @@
 import models
-
+Guitar = models.Guitar
 from flask import Blueprint, request, jsonify
 from playhouse.shortcuts import model_to_dict
 
 guitars = Blueprint('guitars', 'guitars')
 
 @guitars.route('/', methods=['GET'])
-def hello_guitar():
-  result = models.Guitar.select()
+def index_guitar():
+  result = Guitar.select()
   data = []
   count = 0
   for guitar in result:
@@ -19,11 +19,19 @@ def hello_guitar():
     status=200
     ), 200
 
+@guitars.route('/<id>', methods=['GET'])
+def find_guitar(id):
+  result = Guitar.select().where(Guitar.id==id)
+  return jsonify(
+    data=model_to_dict(result[0]),
+    message=f"Successfully found 1 results",
+    status=200
+    ), 200
 
 @guitars.route('/', methods=['POST'])
 def create_guitar():
   payload = request.get_json()
-  new_guitar = models.Guitar.create(name=payload['name'], is_electric=payload['is_electric'], price=payload['price'])
+  new_guitar = Guitar.create(name=payload['name'], is_electric=payload['is_electric'], price=payload['price'])
   
   guitar_dict = model_to_dict(new_guitar)
   return jsonify(
