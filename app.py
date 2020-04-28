0cbd91c721c5594587722d4b7d9afd1574d03d85
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-27 21:08:16 -0400

    passing CORS to local

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from resources.guitars import guitars
+from flask_cors import CORS
 
 import models
 
@@ -8,7 +9,9 @@ DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+CORS(guitars, origins=['http://localhost:3000'], supports_credentials=True)
 app.register_blueprint(guitars, url_prefix='/api/v1/guitars')
+
 
 @app.route('/')
 def hello():
